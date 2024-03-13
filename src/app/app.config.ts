b476c55854f3bd55a06da88e8e7837c6bd9c583a
Damiano Maka <133500319+damiano-maka@users.noreply.github.com>
2024-03-13 19:36:46 +0100

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,15 +8,7 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBuOXA2dVh7Iim3oEZ_wLs4xmu6Sq7RIdw',
-  authDomain: 'lab-tv-movie.firebaseapp.com',
-  databaseURL:
-    'https://lab-tv-movie-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'lab-tv-movie',
-  storageBucket: 'lab-tv-movie.appspot.com',
-  messagingSenderId: '305543977569',
-  appId: '1:305543977569:web:cbde7a5377d0bdfee46820',
-  measurementId: 'G-58C8VE42GN',
+  /* firebase config api key and others */
 };
 
 export const appConfig: ApplicationConfig = {
@@ -24,10 +16,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
     provideAuth0({
-      domain: 'dev-hgt3pxxduftflrlo.us.auth0.com',
-      clientId: 'VClVLVEV1X68ojkDjs3oTpowAu9EjE2v',
+      domain: 'yourt auth0 domain',
+      clientId: ' your client id ',
       authorizationParams: {
-        redirect_uri: 'https://damiano-maka.github.io/auth0/',
+        redirect_uri: 'your site',
         audience: 'http://public-api/',
         scope:
           'openid profile email offline_access read:roles read:users read:logs ',
